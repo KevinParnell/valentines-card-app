6958a21162551eacd4699add8e49bff4bf20850a
Kevin Parnell <kpbballer@gmail.com>
2016-02-02 08:38:35 -0500

    first commit

---
+++ app/src/main/java/com/example/kevin/kevinsvalentinecard/valentinecard.java
@@ -0,0 +1,19 @@
+package com.example.kevin.kevinsvalentinecard;
+
+import android.graphics.PointF;
+import android.opengl.Matrix;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+public class valentinecard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valentinecard);
+    }
+}
+
+
